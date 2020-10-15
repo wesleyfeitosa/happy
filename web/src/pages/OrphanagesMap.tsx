3568d5e78c82f5d55756dcf870faffd9e47aee49
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-10-14 22:06:14 -0300

    [Web] - third day

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+import mapIcon from '../utils/mapIcon';
+
+import '../styles/pages/orphanages-map.css';
+import api from '../services/api';
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
+const OrphanegesMap: React.FC = () => {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('/orphanages').then((response) => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Local no Mapa do Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Quixeramobim</strong>
+          <span>Ceará</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-5.180384, -39.2866842]}
+        zoom={15}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        {orphanages.map((orphanage) => (
+          <Marker
+            icon={mapIcon}
+            position={[orphanage.latitude, orphanage.longitude]}
+            key={orphanage.id}
+          >
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxwidth={240}
+              className="map-popup"
+            >
+              {orphanage.name}
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#FFF" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+};
+
+export default OrphanegesMap;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
-import OrphanegesMap from './pages/OrphanegesMap';
+import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />
-        <Route path="/app" component={OrphanegesMap} />
+        <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
